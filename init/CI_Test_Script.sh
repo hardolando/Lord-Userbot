145c2f87024c06faa3d1018ed8b6ee570fcd68ec
I'm Papanda <86427308+hardolando@users.noreply.github.com>
2021-09-06 18:14:33 +0700

    Update CI_Test_Script.sh

--- init/CI_Test_Script.sh
+++ init/CI_Test_Script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# Copyright (C) 2020 The Raphielscape Company LLC.
+# Copyright (C) 2021 papahri asv group LCC
 #
-# Licensed under the Raphielscape Public License, Version 1.d (the "License");
+# Licensed under the Papahri Public License, Version 1.d (the "License");
 # you may not use this file except in compliance with the License.
 #
 # CI Runner Script for Paperplane CI
