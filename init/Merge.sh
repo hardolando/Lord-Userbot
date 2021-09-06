72404b243921c0282d74c2e303a151f3f7ca4cbf
I'm Papanda <86427308+hardolando@users.noreply.github.com>
2021-09-06 18:17:59 +0700

    Update Merge.sh

--- init/Merge.sh
+++ init/Merge.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# Copyright (C) 2020 The Raphielscape Company LLC.
+# Copyright (C) 2021 papahri Company LLC.
 #
-# Licensed under the Raphielscape Public License, Version 1.d (the "License");
+# Licensed under the papahri Public License, Version 1.d (the "License");
 # you may not use this file except in compliance with the License.
 #
 # CI Runner Script for Paperplane CI
